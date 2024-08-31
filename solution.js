2cbd7b657e62aabc95f49c9a5d68ab07164cab47
Cesar Nsingi <cesarricardo1805@hotmail.com>
2024-08-31 13:45:02 +0400

    graded attempt

--- solution.js
+++ solution.js
@@ -1,87 +1,75 @@
 // Given variables
 const dishData = [
     {
-        name: "Italian pasta",
-        price: 9.55
+      name: "Italian pasta",
+      price: 9.55,
     },
     {
-        name: "Rice with veggies",
-        price: 8.65
+      name: "Rice with veggies",
+      price: 8.65,
     },
     {
-        name: "Chicken with potatoes",
-        price: 15.55
+      name: "Chicken with potatoes",
+      price: 15.55,
     },
     {
-        name: "Vegetarian Pizza",
-        price: 6.45
+      name: "Vegetarian Pizza",
+      price: 6.45,
     },
-];
-const tax = 1.20;
-
-// Implement getPrices()
-function getPrices(taxBoolean) {
-    for (let i = 0; i < dishData.length; i++) {
-        let finalPrice; 
-
-        if (taxBoolean === true) {
-            finalPrice = dishData[i].price * tax;
-        }
-
-        else if (taxBoolean === false) {
-        finalPrice = dishData[i].price;
-        }
-    
-        else {
-            console.log("You need to pass a boolean to the getPrices call!");
-        return; 
-        }
-        
-        console.log("Dish:" + dishData[i].name + " Price: $ " + finalPrice.toFixed(2));
-    } 
-
-}
-
-// Implement getDiscount()
-function getDiscount(taxBoolean, guests) {
+  ];
+  const tax = 1.2;
+  
+  // Implement getPrices()
+  function getPrices(taxBoolean) {
+    for (menu of dishData) {
+      // console.log(menu); this is for testing the code
+      let finalPrice;
+      if (taxBoolean == true) {
+        finalPrice = menu.price * tax;
+      } else if(taxBoolean == false){
+        finalPrice = menu.price;
+      }
+      else {
+        console.log("You need to pass a boolean to the getPrices call!");
+        return;
+      }
+  
+      console.log(`Dish: ${menu.name} Price: $${finalPrice}`);
+      // console.log("Dish: " + menu.name + "Price: $" + finalPrice)
+    }
+  }
+  
+  // Implement getDiscount()
+  function getDiscount(taxBoolean, guests) {
     getPrices(taxBoolean);
-
-    if (typeof guests === 'number' && guests > 0 && guests < 30) {
-        let discount = 0; 
-
-        if (guests < 5) {
-            discount = 5;
-        } else if(guests >= 5){ 
-            discount = 10;
-        }
-
-        console.log('Discount is: $' + discount);
+  
+    if (typeof guests == "number" && guests > 0 && guests < 30) {
+      let discount = 0;
+  
+      if (guests < 5) {
+        discount = 5;
+      }
+      else {
+        discount = 10;
+      }
+  
+      console.log('Discount is: $' + discount);
+  
+    } else {
+      // step 12 code here
+      console.log('The second argument must be a number between 0 and 30');
+  
     }
-    
-    else {
-        console.log('The second argument must be a number between 0 and 30');
-    
-    }
-}
-
-// Call getDiscount()
-getDiscount(true, 2);
-getDiscount(false, 10);
+  }
+  
+  // Call getDiscount()
+  getDiscount(true, 2)
+  getDiscount(false, 10)
 
 
 
-// output this test failed 
-/*Dish:Italian pasta Price: $ 11.46
-Dish:Rice with veggies Price: $ 10.38
-Dish:Chicken with potatoes Price: $ 18.66
-Dish:Vegetarian Pizza Price: $ 7.74
-Discount is: $5
-Dish:Italian pasta Price: $ 9.55
-Dish:Rice with veggies Price: $ 8.65
-Dish:Chicken with potatoes Price: $ 15.55
-Dish:Vegetarian Pizza Price: $ 6.45
-Discount is: $10 */ 
 
+  
 
 
 
